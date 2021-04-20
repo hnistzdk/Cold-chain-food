176b3bb57a90a50401f38b6ead9eb442fe61cb76
hnist <369365576@qq.com>
2021-04-20 21:41:59 +0800

    4.20

--- src/main/java/com/zdk/service/UserService.java
+++ src/main/java/com/zdk/service/UserService.java
@@ -5,11 +5,21 @@ import com.zdk.dto.AdminMeta;
 
 import java.util.List;
 
+/**
+ * @author zdk
+ */
 public interface UserService {
+    /**
+    * @param id
+    * @param password
+    * @return int
+     *
+    */
+
     int login(String id, String password);
+
     int enterpriseLogin(String id, String password);
     int adminLogin(String id,String password);
     List<AdminMeta> getAdminList(Integer pageNum, Integer pageSize);
     int adminTotalPage();
-
 }
