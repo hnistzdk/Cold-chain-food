86e95109d0263246b1f9253a0ccb25d3c84f5470
zzm <957957209@qq.com>
2021-04-21 20:08:33 +0800

    4.20

--- src/components/user/Home.vue
+++ src/components/user/Home.vue
@@ -26,7 +26,7 @@
               <span>{{ item.authName }}</span>
             </template>
             <!--            二级菜单-->
-            <el-menu-item :index="'/'+subItem.path+''" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/'+subItem.path)" >
+            <el-menu-item :index="'/'+subItem.path+''" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/'+subItem.path)">
               <template slot="title">
                 <!--              图标-->
                 <i class="el-icon-menu"></i>
@@ -34,14 +34,13 @@
                 <span>{{ subItem.authName }}</span>
               </template>
 
-
-              <!--            三级菜单-->
-              <el-menu-item :index="'/'+subItem1.path+''" v-for="subItem1 in subItem.children" :key="subItem1.id"  >
+              <el-menu-item :index="'/'+subItem.path+''" v-for="subItem1 in subItem.children" :key="subItem1.id" >
                 <template slot="title">
+
+                  <!--              文本-->
                   <span>{{ subItem1.authName }}</span>
                 </template>
-              </el-menu-item>
-
+                </el-menu-item>
             </el-menu-item>
 
           </el-submenu>
