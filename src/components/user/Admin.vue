69a67a13328ab7c84460b84e08ee2206a3d0bacd
zzm <957957209@qq.com>
2021-06-09 11:21:13 +0800

    688

--- src/components/user/Admin.vue
+++ src/components/user/Admin.vue
@@ -31,7 +31,7 @@
 
               <el-switch v-model="scope.row.mg_state" @change="userStateChanged(scope.row)"> </el-switch>
 
-          </template  >
+          </template>
         </el-table-column>
         <el-table-column label="操作" width="200px">
           <template slot-scope="scope">
